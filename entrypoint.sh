691f488eac150413718160c0d4fbcd4050c8c0a7
Saranga Komanduri <skomanduri@civisanalytics.com>
2017-12-19 15:47:21 -0600

    [CIVP-13675] Set GITHUB_PAT environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,6 +50,8 @@ install_deps() {
 
 run_shiny_app() {
   export R_LIBS="${R_LIBS:-$LIB_DIR}"
+  export GITHUB_PAT="${GITHUB_PAT:-$REPO_HTTP_TOKEN}"
+
   ABSOLUTE_APP_DIR="$APP_DIR/$REPO_PATH_DIR"
   exec R -e "shiny::runApp(appDir='$ABSOLUTE_APP_DIR', port=3838, host='0.0.0.0')"
 }
